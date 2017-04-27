8212a2001991d229f5d1160e1b54075b593bf607
Linkyx <18829290053@139.com>
2017-04-27 11:56:31 +0800

    添加mysql链接

--- zeus/settings.py
+++ zeus/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'zeus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zeus',
+        'USER': 'root',
+        'PASSWORD': 'ffml811.',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
